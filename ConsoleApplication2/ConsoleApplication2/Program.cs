b32766857cc9ace985a20dd24af0f191119eaf4f
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-08 16:57:18 -0700

    Flow control program using if/elseif and a while loop 8/8/2016

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberCounter = 0;
+            while (numberCounter < 100) 
+            {
+                numberCounter++;
+                if (numberCounter % 5 == 0 && numberCounter % 3 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (numberCounter % 5 == 0) 
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if (numberCounter % 3 == 0)
+                {
+                    Console.WriteLine("fiz");
+                }
+                else
+                {
+                    Console.WriteLine(numberCounter);
+                }
+                      
+               }
+            Console.ReadLine();
+            }
+
+
+
+
+
+
+        }
+}
