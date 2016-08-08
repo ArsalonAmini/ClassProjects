23de3547939056cc63cca8652131d1929664ae2e
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-08 11:56:05 -0700

    program to create a radioclock from a clock class with functions

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//This is a DevCode Camp program assigment to create a Clock, Radio or fan
+//It is written by Arsalon N Amini-Hajibashi on August 8, 2016
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timekeeper radioclock = new Timekeeper();
+            int time;
+            bool isOn;
+            string color;
+
+            yourradio = radioclock.ChangeTime();
+            Console.WriteLine(yourradio);
+
+
+
+        }
+    }
+}
