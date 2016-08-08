d4e15c004d261b17b8976e1e5c20f9c33c60cab5
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-08 13:49:10 -0700

    debugging and working program including a class, multiple instances of that class and methods/actions on those objects (instances of the classes)

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -7,22 +7,39 @@ using System.Threading.Tasks;
 //This is a DevCode Camp program assigment to create a Clock, Radio or fan
 //It is written by Arsalon N Amini-Hajibashi on August 8, 2016
 
-
+//namespace is a loose term encompassing lists of all class 
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Timekeeper radioclock = new Timekeeper();
-            int time;
-            bool isOn;
-            string color;
 
-            yourradio = radioclock.ChangeTime();
-            Console.WriteLine(yourradio);
+            //creating multiple instances (objects) of the Timekeeper class (clock blueprint)
+            Timekeeper radioclock = new Timekeeper("green", false, 1230);
+            Timekeeper alarmclock = new Timekeeper("black",true, 130);
+            Timekeeper woodenclock = new Timekeeper("yellow", false, 230);
 
+            //Calling function to change color
+            //a function (ChangeTime) on the instances (objects) of the Timekeeper class (blueprint)
+            string  newColorRadio = radioclock.ChangeColor("red");
+            string newColorAlarm = alarmclock.ChangeColor("blue");
+            string newColorWooden = woodenclock.ChangeColor("green");
 
+            //call function to change status
+            bool newStatusRadio = radioclock.ChangeStatus(true);
+            bool newStatusAlarm = alarmclock.ChangeStatus(false);
+            bool newStatusWooden = woodenclock.ChangeStatus(true);
+
+            //call function to change time
+            int newTimeRadio = radioclock.ChangeTime(1230);
+            int newTimeAlarm = alarmclock.ChangeTime(120);
+            int newTimeWooden = woodenclock.ChangeTime(340);
+
+            //output result to command window for user
+            Console.WriteLine(newColorRadio, newStatusRadio, newTimeRadio);
+            Console.WriteLine(newColorAlarm, newStatusAlarm, newTimeAlarm);
+            Console.WriteLine(newColorWooden, newStatusWooden, newTimeRadio);
 
         }
     }
---
+++ ConsoleApplication1/ConsoleApplication1/TimeKeeper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Pseudocode
+// create new class named TimeKeeper
+//create member variables in timkeeper such as...
+//color (string)
+//on or off (boolean)
+//time (int)
+//Create member methods
+//change time (add 30 to time)(int)
+//turn on/off (boolean)
+
+
+namespace ConsoleApplication1
+{
+    
+    //giving initial values to the class attributes
+    
+
+    //creating a class (object blueprint) called TimeKeeper. Use TimeKeeper to create specific clock colors, on/off, and time
+    public class Timekeeper
+    {
+
+       //constructor member variable (attribute) initial setting / calibration
+        string color = "blue"; //color set to blue
+        bool isOn = false; // logic gate turned to off //clock is off
+        int time = 1230; //int data type time set to 1230
+
+       //constructor function //special method called first to set member variables of objects //pass in three data types as args
+        public Timekeeper(string Color, bool IsOn, int Time) //call function with 
+        {
+            color = Color; 
+            isOn = IsOn;
+            time = Time;
+        }
+        //function (method) for changing the color
+        public string ChangeColor (string Color)
+        {
+            color = Color;
+
+            return color;
+        }
+        //Function for changing on/off status (turning it on/off)
+        public bool ChangeStatus (bool IsOn)
+        {
+            isOn = IsOn;
+            return isOn;
+        }
+        //Function for changing Time
+        public int ChangeTime(int Time)
+        {
+            time = Time;
+            time = time + 30;
+            return time;
+        }
+        
+
+    }
+    
+}
